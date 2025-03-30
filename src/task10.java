f8ff5b3e284fbf321d77826b48a5a75a6b2b4ff4
ayashayaa <153350006+ayashyaa@users.noreply.github.com>
2025-03-30 20:16:55 +0500

    10

---
+++ src/task10.java
@@ -0,0 +1,13 @@
+public class task10 {
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(gcd(32, 48));
+        System.out.println(gcd(10, 7));
+    }
+}
