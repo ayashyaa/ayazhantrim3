ea689c9860fcbc122b00846779d37c36622d9266
ayashayaa <153350006+ayashyaa@users.noreply.github.com>
2025-03-30 21:16:04 +0500

    modified

--- src/task2.java
+++ src/task2.java
@@ -1,14 +1,19 @@
 public class task2 {
+
+    // Recursive function to find the sum of array elements
     public static double findSum(int[] arr, int n) {
-        if (n == 1) {
+        if (n == 1) { //Base case
             return arr[0];
         }
+
+        // adding the last element to the sum of the remaining array
         return arr[n - 1] + findSum(arr, n - 1);
     }
 
+    // this function calculates the average of array elements
     public static double findAverage(int[] arr, int n) {
-        if (n == 0) return 0;
-        return findSum(arr, n) / n;
+        if (n == 0) return 0; //edge case, to avoid division to 0
+        return findSum(arr, n) / n; //using sum to compute the average
     }
 
     public static void main(String[] args) {
